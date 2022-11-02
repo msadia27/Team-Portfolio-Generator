57262ec8e2a9ffd2d54b81c6dc3699a0191a83ca
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-02 00:10:44 -0400

    added function for promptquestion, buildhtml, and arrays with html file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const inquirer = require("inquire");
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
@@ -6,10 +6,11 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const team = [];
 
 //initial employee questions
 const questions = () => {
-    answer.inquirer.prompt([
+    inquirer.prompt([
     {
         //name
     type: "input",
@@ -35,19 +36,78 @@ const questions = () => {
     message: "What is employee position/role?", 
     choices: [Manager, Engineer, Intern],  
     }
-    ])
+    ]); 
+}
+
+function promptQuestions() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        if (answers.role === Manager) {
+            // officeNumber
+            const officeNumber = inquirer.prompt ([ 
+            {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number?",
+            }
+            ]);
+            const addManager = new Manager(answers.name, answers.id, answers.email, officeNumber.officeNumber);
+            team.push(
+                new Manager(answers.name, answers.id, answers.email, officeNumber.officeNumber)
+                );
+            } else if (answers.role === Engineer) {
+        //Engineer additional questions
+        // github // GitHub username
+            const github= inquirer.prompt ([ 
+            {
+            type: "input",
+            name: "github",
+            message: "What is your github?",
+            }
+            ]);
+            const addEngineer = new Engineer(answers.name, answers.id, answers.email, github.github);
+            team.push(new Engineer(answers.name, answers.id, answers.email, github.github));
+            } else if (answers.role === Intern) {
+        //intern additional questions
+        // school
+            const school = inquirer.prompt ([ 
+            {
+            type: "input",
+            name: "school",
+            message: "What school did intern go too?",
+            }
+            ]);
+            const addIntern = new Intern(answers.name, answers.id, answers.email, school.school);
+            team.push(new Intern(answers.name, answers.id, answers.email, school.school));
+        };
+    })
+    buildTeam()
 };
 
-//Manager additional questions
-// officeNumber
-// getRole() // Overridden to return 'Manager'
+promptQuestions();
 
-//Engineer additional questions
-// github // GitHub username
-// getGithub()
-// getRole() // Overridden to return 'Engineer'
+//create a function to build team in the HTML file
+function buildTeam() {
+    {
+        fs.writeFile("./dist/index.html", team, function(err){
+            console.log(team)
+            if(err){
+                console.log(err)
+            } else {
+                console.log("HTML file made")
+            }
+        });
+    }
+};
 
-//intern additional questions
-// school
-// getSchool()
-// getRole() // Overridden to return 'Intern'
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function(team){
+        writeToFile("./dist/index.html", buildTeam(team))
+        console.log(team)
+    })
+};
+
+//function call to initialize app
+init();
